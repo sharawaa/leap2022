5653b98ef2458af0e781ed38035eb9a067e87e55
Morning <morning@admins-iMac.local>
2022-11-28 13:39:18 +0800

    java calcultor

---
+++ JS/function.js
@@ -0,0 +1,79 @@
+let mycode ="sharawaa";
+console.log(mycode);
+
+
+let a = 6 ;
+let b = 5 ;
+
+
+
+// let Addition = "Addition of given 2 Numberbers is 11"
+// let Subtraction = "Subtraction of given 2 Numberbers is 1"
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+
+let d = 75 ;
+
+console.log( (d-(d%10))/10);
+
+
+// let num1 = parseFloat(prompt("ehnii tooo"));
+// let num2 = parseFloat(prompt("hoyr dahin tooo"));
+// alert("2 toonii niiber n: " + (num1+num2));
+// alert(" ehnii toog 2 dahi toond uldegdel ni: " + (num1%num2));
+
+
+// MyAge, yourAge нэртэй хувьсагч зарлан эхний утгыг оноох 
+let myAge = 22;
+let yourAge=19;
+
+
+
+/* gurvaljinii perimeter*/
+let num3 = 27;
+let num4 = 33; 
+let num5 = 13;
+  
+    console.log("Perimeter=" + (num3 + num4 + num5));
+
+
+    /* toirgiin talbai*/
+
+    let radius = 15;
+    const PI = 3.14;
+    console.log("gurvaljnii talbai=" + (PI * radius * radius));
+
+    // m = y2-y1/x2-x1 хариуг олох 
+
+    let x = 5;
+    let y = 10;
+
+    console.log("m= " + (y*2-y*1/x*2-x*1))
+    
+    // 15 МB дата 3 секундэд манай интернэт онлайнаар татаж чаддаг бол түүнийг BIT Rate-ийг олно уу
+
+    let mb = 15;
+    let bit = 1.1920928955078E7 ;
+    let second = 3;
+
+console.log((mb*bit)/second+" s/bit");
+
+
+// gurvaljniii gurvan tal ugugdsun uyd talbaig ol
+
+let t = 3;
+let n = 4;
+let c = 6;
+let s = (t+n+c)/2;
+
+let TrngleS = Math.sqrt(s*(s-t)*(s-n)*(s-c))
+console.log(TrngleS+" s");
+
+
+
