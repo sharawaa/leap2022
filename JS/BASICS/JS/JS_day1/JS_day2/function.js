6802a481fc5f4b2ba6e533e668d8bea9f6ae77dd
Morning <morning@admins-iMac.local>
2022-11-29 12:47:19 +0800

    auwgf[q0wreh

---
+++ JS/BASICS/JS/JS_day1/JS_day2/function.js
@@ -0,0 +1,157 @@
+// let a =15;
+// let b = 20;
+
+// console.log(a<b)
+
+
+// let myAge = 22;
+
+// if(myAge >= 21){
+//     console.log("zaa yahav orhiig zuvshuurii")
+// }
+// else{
+//     console.log("orj bolkuee hugshuun"
+//     )
+// }
+
+// let day = -6;
+// if (day === 1){
+// console.log("davaa garag")
+// }
+// else if(day === 2){
+//     console.log("myagmar garag")
+// }
+// else if(day === 3){
+//     console.log("lhagva garag")
+// }
+// else if(day === 4){
+//     console.log("purev garag")
+// }
+// else if(day === 5){
+//     console.log("baasa garag")
+// }
+// else if(day === 6){
+//     console.log("byamba garag")
+// }
+// else if(day === 7){
+//     console.log("nyam garag")
+// }
+// else if(day === 7){
+//     console.log("nyam garag")
+// }
+
+// else{
+//     console.log("buruu too ")
+// }
+
+// let day = 4;
+// let weekName ;
+
+// if(day<1 && day>7){
+//     console.log=("error")
+// }
+
+// if (day === 1){
+// weekName="davaa garag"
+// }
+// else if(day === 2)
+// {
+//     weekName="myagmar garag"
+// }
+// else if(day === 3){
+//     weekName="lhagva garag"
+// }
+// else if(day === 4){
+//     weekName="purev garag"
+// }
+// else if(day === 5){
+//     weekName="baasa garag"
+// }
+// else if(day === 6){
+//     weekName="byamba garag"
+// }
+// else if(day === 7){
+//     weekName="nyam garag"
+// }
+// else if(day === 7){
+//     weekName="nyam garag"
+// }
+// console.log(weekName)
+
+
+
+
+
+//  let weekday = 4;
+// switch ( weekday){
+//     case 1:
+//         console.log("mon");
+//     break;
+//     case 2:
+//         console.log("thue");
+//     break;
+//     case 3:
+//         console.log("wed");
+//     break;
+//     case 4:
+//         console.log("thu");
+//     break;
+//     case 5:
+//         console.log("fri");
+//     break;
+//     case 6:
+//         console.log("sat");
+//     break;
+//     case 7:
+//         console.log("sun");
+//     break;
+//     default:
+//         console.log("not a day")
+
+// }
+
+// switch(weekday){
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("ajliin udur")
+//         break;
+//     case 6:
+//     case 7:
+//         console.log("amraltiin udur")
+//         break;
+// }
+// if(weekday==1 || weekday==2 || weekday==3 || weekday==4 || weekday==5){console.log("working day")}
+
+
+
+
+
+let x = 10; 
+let y = 30 ;
+let z = 20;
+if(x>y){
+    if(x>z){
+        console.log(x, " n hamgiin ih ");
+    }
+
+else{
+        console.log(y, "n hamgiiin ih ");
+    }
+}
+    else{console.log(z, "n hamgiin ih ");
+
+    }
+
+    /* ternary */
+    let age = 17;
+    age<21 ? console.log("chi arhi uuj bolohgue ahiin duu"): console.log("taaruulj uugaarain hugshuun");
+
+    Condition ? true : false;
+
+    const myState = age >=10 && age <=18 ? "teenage ": "adult";
+
+
+    
