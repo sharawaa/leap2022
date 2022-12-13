e4035cb728e3c857f9543c9d6f452a73a3cc0efa
sharawnyambuu urtnasan <sharawnyambuuurtnasan@sharawnyambuus-MacBook-Air.local>
2022-12-13 16:46:03 +0800

    commit

---
+++ JS/challe_js/FUNCTION_js/tegsh_sondgoi_ologch/function.js
@@ -0,0 +1,22 @@
+/*1. 2 тоон утга авдаг 2 function үүсгэ.
+2. 2 авсан тоон утгын хоорондох тэгш тоонуудын array буцаадаг 1 function.
+2. 2 авсан тоон утгын хоорондох сондгой тоонуудын array буцаадаг 1 function. */
+
+
+let num1 = 10;
+let num2 = 20;
+let output = [];
+
+even(num1,num2)
+function even(a, b) {
+    let j = 0;
+    for (let i = a; a <= b; i++) {
+        if (i % 2 == 0) {
+            output[j] = i;
+            j++
+        }
+
+    }
+    return output
+}
+ console.log(output)
