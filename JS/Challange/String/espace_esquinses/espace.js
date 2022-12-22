7a77a611b4cac9d87b7f92bdefa07e827dd7efec
sharawnyambuu urtnasan <sharawnyambuuurtnasan@sharawnyambuus-MacBook-Air.local>
2022-12-22 09:09:32 +0800

    macaas

--- JS/Challange/String/espace_esquinses/espace.js
+++ JS/Challange/String/espace_esquinses/espace.js
@@ -1,8 +1,2 @@
-/*1 удаагийн хэвлэлтээр үүнийг хэвлэж гаргана уу.
 
-Hello I am "Name Here".
-I am 'Age here' years old.
-    This is the new line with tab and some \backslashes\ */
-    let p = "Hello I am Name Here";
-    let o ="I am Age here years old."
-    console.log("Hello I am Name Here\nI am Age here years old.\n\tThis is the new line with tab and some backslashes\.");
+`   `
--- JS/Challange/hack_this_site/function.js
+++ JS/Challange/hack_this_site/function.js
@@ -1,16 +1,11 @@
-/*hacmk hiih code bichih */
-//ehniii hack 
-
-//2 too oruulna ter toon hoorondoh tegsh toog hevlene
-
-//TEXT dundaas tom usgiig ni ylgaj avah ter ni pass code bnaa
-
 let text = document.querySelector(".text").innerText;
-let bigwords = "QWERTYUIOPLKJHGFDSAZXCVBNM";
-let output=""
- for(i=0;i<text.length;i++){
-    for(j=0;j<bigwords.length;j++){
+let bigwords = "QWERTYUIOPASDFGHJKLZXCVBNM";
+let output = ""
+for(i=0;i<text.length;i++){
+    for(j=0; j<bigwords.length;j++){
         if(text[i]==bigwords[j]){
-            output= output+text[i]+"";
-        }}}
- console.log(output);
+            output=output + text[i]+"";
+        }
+    }
+}
+console.log(output)
