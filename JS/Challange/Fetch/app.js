1d67d92e270ec710c65590cadad54b12e0612969
leap2022 <sharawnyambuuurtnasan@gmail.com>
2022-12-27 09:44:15 +0800

    macaas1

--- JS/Challange/Fetch/app.js
+++ JS/Challange/Fetch/app.js
@@ -1,30 +1,77 @@
-const select =document.querySelector("#breeds");
-const card = document.querySelector("#card");
-let current= "";
+// const select =document.querySelector(`#breeds`);
+// const card = document.querySelector(`#card`);
+// let current= "";
+
+// fetch("https://dog.ceo/api/breeds/list")
+//     .then((res)=>res.json())
+//     .then((data) => generator(data.message)
+//         // fetchImage(data.message[0]);
+//         // current= data.message[0];
+//     );
+
+
+// function generator(data){
+//     data.map((dogBreed) => {
+//         const html =`<option value="${dogBreed}">${dogBreed}</option>`;
+//         select.innerHTML+=html;
+//     });
+// }
+
+
+
+// //
+
+// function generatorImage(url){
+//     const image = `<img scr="${url}">`
+//     card.innerHTML = image
+// }
+
+
+
+// function fetchImage(breedType){
+//     fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+//     .then((res)=>res.json())
+//     .then((data) => generatorImage(data.message));
+// }
+
+// select.addEventListener("change", () => {
+//     fetchImage(select.value);
+// });
+// select.addEventListener("click", () => {
+//     fetchImage(select.value)
+// });
+
+
+const select = document.querySelector("#breeds");
+const card = document.querySelector(".card");
+let current = "";
 
 fetch("https://dog.ceo/api/breeds/list")
-    .then((res)=>res.json())
-    .then((data) => {
-        generateBreeds(data.message);
-        fetchImage(data.message[0]);
-        current= data.message[0];
-    });
+  .then((res) => res.json())
+  .then((data) => {
+    generateBreeds(data.message);
+    // console.log(data);
+    fetchImage(data.message[0]);
+    current = data.message[0];
+  });
 
-
-function generateBreeds(data){
-    data.map((dogBreed) => {
-        const html =`<option value="${dogBreed}">${dogBreed}</option>`;
-        select.innerHTML+=html;
-    });
+function generateBreeds(data) {
+  data.map((dogBreed) => {
+    const html = <option value="${dogBreed}">${dogBreed}</option>;
+    select.innerHTML += html;
+  });
 }
 
+function generateImage(url) {
+    const image = `<img src="${url}">`;
+    card.innerHTML = image;
+  }
+  
 
-function generateImages(url){
-    fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
-    .then((res)=>res.json())
-    .then((data) => {
-        renderCountry(data[0])
-        const neighbour=1
-    })
-    
-}
+  function fetchImage(breedType) {
+    fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+      .then((res) => res.json())
+      .then((data) => generateImage(data.message))
+      
+  }
+
