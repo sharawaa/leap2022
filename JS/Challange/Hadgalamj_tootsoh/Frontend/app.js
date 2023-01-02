b306cd0614a7412994776a9f93c7d9771cb27428
leap2022 <sharawnyambuuurtnasan@gmail.com>
2023-01-02 09:02:51 +0800

    2023 onii anhnii push

--- JS/Challange/Hadgalamj_tootsoh/Frontend/app.js
+++ JS/Challange/Hadgalamj_tootsoh/Frontend/app.js
@@ -1,51 +1,48 @@
-fetch("http://localhost:3333/api/users")
-.then((res)=> res.json())
-.then((data)=>{ users(data)
-})
+// fetch("http://localhost:3333/api/users")
+// .then((res)=> res.json())
+// .then((data)=>{ users(data)
+// })
 
 
 
 
-function users(data){
-  data.map((user)=>{
+// function users(data){
+//   data.map((user)=>{
 
-    let newDiv = document.getElementById("card");
+//     let newDiv = document.getElementById("card");
    
-    newDiv.innerHTML += `<div>
- 
-    <div> ${user.surName} </div>
-    <div> ${user.name} </div>
-    <div> ${user.balance} </div>
-    <div> ${user.balance_type}</div>
+    
+//   })
+// }
 
-    </div>`;
-  })
-}
+let button = document.querySelector("button");
+let input = document.querySelector("input");
 
-let btn = document.getElementById("btn");
 
-let input = document.getElementById("input");
 
-btn.addEventListener("click", ()=>{
+button.addEventListener("click", () => {
+  // users.innerHTML =""
   fetch("http://localhost:3333/api/users")
   .then((res)=> res.json())
-  .then((data)=>{ users(data)
-    data.innerHTML =""
-console.log(input.value)
-
-  for (i=0;i<input.value;i++){
-    
+  .then((data)=>{ 
     data.map ((user)=>{
-      if(user.balance_type){
-      let huu = input.value*(user.balance*0.1)+user.balance;
-      
-       console.log(huu)
-      }else {
-        let huu2 =input.value * (user.balance*0.01)+user.balance 
-        console.log(huu2)
-      }
+      let dollor = user.balance;
+        for (i=0; i<input.value; i++){
+          if(user.balance_type){
+           dollor = dollor * 0.1 + dollor;
+            console.log(dollor)
+          }else {
+            dollor = dollor * 0.01 + dollor 
+            console.log(dollor)
+        }
+    }
+    user.innerHTML += `<div class = "user">
+    <div class = "sureaName">sureName: ${user.surName} </div>
+    <div class = "name">name:> ${user.name} </div>
+    <div class = "balance">balance:> ${user.balance} </div>
+   
+    </div>`;
       })
-  }
-  })
+})
 })
 
